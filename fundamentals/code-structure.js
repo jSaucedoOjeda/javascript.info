e1504a1035a49d8485d11b5920af90762f582aba
jSaucedo <saucedo.jesus@sonora.edu.mx>
2020-12-28 18:28:28 -0700

    code structure

---
+++ fundamentals/code-structure.js
@@ -0,0 +1,37 @@
+/**
+ * Statements
+ * 
+ * Statements are syntax constructs and commands that perform actions
+ * Statements can be separated with a semicolon.
+ */
+
+    //This is a statement and we can have as many statements in our code as we want.
+    alert('Hello');
+
+    //Usuarlly, statements are written on separate lines to make the code more readable:
+    alert('Hello');
+    alert('World')
+
+    
+    //Semicolon can be omitted when a line break exists. 
+    //Javascript interprets the linea break as an implicit semicolon.
+    
+
+    //There are cases when a newline does not mean a semicolon. For example:
+    alert(3 +
+        1
+        + 2);
+
+    //This happens because it's a incomplete expression. 
+    //We recommend putting semicolons between statements even if they are separated by newlines.
+ 
+/**
+ * Comments
+ * The content of comments is ignored, so if we put code inside , it wont execute
+ */
+
+ //One line comment start with two forward slash characters.
+ 
+ /* Multiline comments
+  start with a forward 
+  slash and an asterisk */
